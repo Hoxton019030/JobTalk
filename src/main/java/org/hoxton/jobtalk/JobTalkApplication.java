9bb7a52a215f4e434775b59ed56ad376f4517156
Hoxton <za546321@yahoo.com.tw>
2024-10-09 09:52:02 +0800

    first

---
+++ src/main/java/org/hoxton/jobtalk/JobTalkApplication.java
@@ -0,0 +1,13 @@
+package org.hoxton.jobtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hoxton/jobtalk/JobTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoxton.jobtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
