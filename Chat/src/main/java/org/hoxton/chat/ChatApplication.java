244646f29de2c90c018f149e52510f2005a1b34e
Hoxton <za546321@yahoo.com.tw>
2024-10-09 20:15:16 +0800

    redis模塊成功

--- Chat/src/main/java/org/hoxton/chat/ChatApplication.java
+++ Chat/src/main/java/org/hoxton/chat/ChatApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication()
-//scanBasePackages = {"org.hoxton.redis.redis", "org.hoxton.chat"}
+@SpringBootApplication(scanBasePackages = {"org.hoxton.redis.redis", "org.hoxton.chat"})
 public class ChatApplication {
 
     public static void main(String[] args) {
--- Chat/src/main/java/org/hoxton/chat/controller/MessageController.java
+++ Chat/src/main/java/org/hoxton/chat/controller/MessageController.java
@@ -11,18 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RequestMapping("/message")
+@RequestMapping("/message")
 @RestController
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class MessageController {
 
-//    private RedisService redisService;
+    private final RedisService redisService;
 
-    public MessageController() {
-        log.info("初始化");
-//        this.redisService = redisService;
-    }
 
     @GetMapping("/test")
     public Response<Message> test() {
@@ -33,7 +29,7 @@ public class MessageController {
         message.setSenderId(0);
         message.setReceiverId(0);
         message.setSendTime(new Date());
-//        redisService.setValue("Test",1,100);
+        redisService.setValue("Test",1,100);
         return Response.success(message);
     }
 }
